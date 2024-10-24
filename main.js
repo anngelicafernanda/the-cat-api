a51cadf93b34ed4855c4036d7410ab8f3498c004
Angelica Contreras <afernandac94@gmail.com>
2024-10-24 14:10:24 -0300

    Header content-type

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const API_URL_RANDOM =
 	'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_OixKTUrsYplf60tg9pJBWxjq3Qm9TXfiubVVOOnkJHF8UkuxCWHQP20bFnLre537';
 const API_URL_FAVORITES =
 	'https://api.thecatapi.com/v1/favourites?api_key=live_OixKTUrsYplf60tg9pJBWxjq3Qm9TXfiubVVOOnkJHF8UkuxCWHQP20bFnLre537';
+const API_URL_DELETE = (id) =>
+	`https://api.thecatapi.com/v1/favourites/${id}?api_key=live_OixKTUrsYplf60tg9pJBWxjq3Qm9TXfiubVVOOnkJHF8UkuxCWHQP20bFnLre537`;
 
 const spanError = document.getElementById('error');
 
@@ -35,19 +37,27 @@ async function loadFavouriteGatitos() {
 	if (res.status !== 200) {
 		spanError.innerHTML = 'Hubo un error: ' + res.status + data.message;
 	} else {
-		data.forEach((Gatito) => {
-			const section = document.getElementById('favoriteGatitos');
+		const section = document.getElementById('favoriteGatitos');
+		section.innerHTML = '';
+		const h2 = document.createElement('h2');
+		const h2Text = document.createTextNode('Gatitos favoritos');
+
+		h2.appendChild(h2Text);
+		section.appendChild(h2);
+		data.forEach((gatito) => {
 			const article = document.createElement('article');
 			const img = document.createElement('img');
 			const btn = document.createElement('button');
 			const btnText = document.createTextNode('Sacar al Gatito de favoritos');
 
-			img.src = Gatito.image.url;
+			img.src = gatito.image.url;
 			img.width = 150;
 			btn.appendChild(btnText);
 			article.appendChild(img);
 			article.appendChild(btn);
 			section.appendChild(article);
+
+			btn.onclick = () => deleteFavoriteGatito(gatito.id);
 		});
 	}
 }
@@ -56,7 +66,9 @@ async function saveFavouriteGatito(id) {
 	const res = await fetch(API_URL_FAVORITES, {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json',
+			'Content-Type': 'text/plane',
+			'X-API-KEY':
+				'live_OixKTUrsYplf60tg9pJBWxjq3Qm9TXfiubVVOOnkJHF8UkuxCWHQP20bFnLre537',
 		},
 		body: JSON.stringify({
 			image_id: id,
@@ -69,6 +81,26 @@ async function saveFavouriteGatito(id) {
 
 	if (res.status !== 200) {
 		spanError.innerHTML = 'Hubo un error: ' + res.status + data.message;
+	} else {
+		console.log('Michi agregado de favoritos');
+		loadFavouriteGatitos();
+	}
+}
+
+async function deleteFavoriteGatito(id) {
+	const res = await fetch(API_URL_DELETE(id), {
+		method: 'DELETE',
+	});
+	const data = await res.json();
+
+	console.log('Delete');
+	console.log(res);
+
+	if (res.status !== 200) {
+		spanError.innerHTML = 'Hubo un error: ' + res.status + data.message;
+	} else {
+		console.log('Michi eliminado de favoritos');
+		loadFavouriteGatitos();
 	}
 }
 
